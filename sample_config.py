bbbbd196d839cdb6692ab91b6659f6fe093e6f07
REX BOTZ <89206214+REX-BOTZ@users.noreply.github.com>
2021-12-25 13:57:10 +0530

    Add files via upload

---
+++ sample_config.py
@@ -0,0 +1,17 @@
+# By @REX_BOTZ
+
+
+import os
+
+LINK = "https://youtu.be/D8lANif4U9A"
+
+class Config(object):
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+
+    APP_ID = int(os.environ.get("APP_ID", 12345))
+
+    API_HASH = os.environ.get("API_HASH", "")
+
+    # Get this api from https://www.remove.bg/b/background-removal-api
+    RemoveBG_API = os.environ.get("RemoveBG_API", "")
+    
---
+++ script.py
@@ -0,0 +1,37 @@
+class script(object):
+
+    START_MSG = """ <b>𝙷𝚒 {}
+
+𝙸'𝚖 𝙸𝚖𝚊𝚐𝚎 𝙴𝚍𝚒𝚝𝚘𝚛 𝙱𝚘𝚝 𝙷𝚎𝚕𝚙𝚜 𝚝𝚘 𝙴𝚍𝚒𝚝 𝙸𝚖𝚊𝚐𝚎 𝚝𝚑𝚛𝚘𝚞𝚐 𝚃𝚎𝚕𝚎𝚐𝚛𝚊𝚖
+
+𝙵𝚘𝚛 𝙼𝚘𝚛𝚎 𝙲𝚕𝚒𝚌𝚔 𝙷𝚎𝚕𝚙....</b>"""
+    HELP_MSG = """𝙷𝚊𝚒, 𝙵𝚘𝚕𝚕𝚘𝚠 𝚝𝚑𝚎 𝚂𝚝𝚎𝚙𝚜 𝚐𝚒𝚟𝚎𝚗 𝙱𝚎𝚕𝚘𝚠..
+
+<b>✪ 𝚂𝚎𝚗𝚍 𝙼𝚎 𝚊𝚗𝚢 𝙸𝚖𝚊𝚐𝚎 𝚝𝚘 𝙴𝚍𝚒𝚝..</b>
+<b>✪ 𝚂𝚎𝚕𝚎𝚌𝚝 𝙲𝚘𝚛𝚛𝚎𝚜𝚙𝚘𝚗𝚍𝚒𝚗𝚐 𝙼𝚘𝚍𝚎 𝚈𝚘𝚞 𝙽𝚎𝚎𝚍</b>
+<b>✪ 𝚈𝚘𝚞𝚛 𝙴𝚍𝚒𝚝𝚎𝚍 𝙸𝚖𝚊𝚐𝚎 𝚆𝚒𝚕𝚕 𝙱𝚎 𝚄𝚙𝚕𝚘𝚊𝚍𝚎𝚍 </b>
+<b>𝙼𝚊𝚒𝚗𝚝𝚊𝚒𝚗𝚎𝚍 𝙱𝚢</b>
+<b>@Rex_Botz</b>"""
+    
+    ABOUT_MSG = """
+╭────[⚡ ᴀʙᴏᴜᴛ ⚡]───⍟
+│
+├<b>🤖 𝙱𝚘𝚝 𝙽𝚊𝚖𝚎 : <a href='https://t.me/ImageEditorRexBot'>ɪᴍɢᴇᴅɪᴛᵖʳᵒ</a></b>
+│
+├<b>📢 𝙲𝚑𝚊𝚗𝚗𝚎𝚕  : <a href='https://t.me/Rex_Botz'>ᴊᴏɪɴ</a></b>
+│
+├<b>👥 𝚅𝚎𝚛𝚜𝚒𝚘𝚗  : <a href='https://t.me/Rex_Bots_Support'>0.9.2ʙᴇᴛᴀ</a></b>
+│
+├<b>💢 𝚂𝚘𝚞𝚛𝚌𝚎   : <a href='https://github.com/REX-BOTZ/'>ᴅᴇᴘʟᴏʏ</a></b>
+│
+├<b>🌐 𝚂𝚎𝚛𝚟𝚎𝚛   : <a href='https://heroku.com'>ʜᴇʀᴏᴋᴜ</a></b>
+│
+├<b>📕 𝙻𝚒𝚋𝚛𝚊𝚛𝚢  : <a href='https://github.com/pyrogram'>ᴘʀɢ1.2.8</a></b>
+│
+├<b>㊙ 𝙻𝚊𝚗𝚐𝚞𝚊𝚐𝚎 : <a href='https://www.python.org'>ᴘʏ3.9.4</a></b>
+│
+├<b>👨‍💻 𝙳𝚎𝚟      : <a href='https://t.me/BENWOLF24'>ᴡᴏʟғ24</a></b>
+│
+├<b>🚸 𝙿𝚘𝚠𝚎𝚛𝚎𝚍  : <a href='https://t.me/FluxPlay'>ғʟᴜxᴘʟᴀʏ</a></b>
+│
+╰──────[ᴛʜᴀɴᴋs 😊]───⍟"""
